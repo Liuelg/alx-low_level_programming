bfdd59ad4c48cb3af760767093b757a83ee62e2c
Liuelg <Liueltekagirma@gmail.com>
2022-12-10 00:16:21 -0800

    sdksjdskdjskdjsdkj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,37 +1,38 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include <stdlib.h>                                                 
+#include <time.h>                                                   
+#include <stdio.h>                                                  
 
 /**                                                                 
  *
- *  * main - main block                                                
-  * Description: Get a random number and print the number            
- * and if it is positive, negative, or zero                         
-  * Return: 0                                                        
- **/                                                                 
+ *main - main block                                                
+ 
+*Description: Get a random number and print the number            
+*and if it is positive, negative, or zero                
+*Return: 0                                                        
+*/                                                                 
 
-int main(void)
+int main(void)                                                      
 
-{
+{                                                                   
 
-		int n;
+	        int n;                                                      
 
+		                                                                    
 
+		        srand(time(0));                                             
 
-			srand(time(0));
+			        n = rand() - RAND_MAX / 2;                                  
 
-				n = rand() - RAND_MAX / 2;
+				        if (n > 0)                                                  
 
-					if(n > 0)
-					         printf("%d is positive\n", n);
-					
-					else if ( n == 0)
-						printf("%d is zero\n", n);
-					
-					else 
-						printf("%d is negative\n", n);
-					
+						                printf("%i is positive\n", n);                      
 
-					return (0);
+					        else if (n < 0)                                             
 
-}
+							                printf("%i is negative\n", n);                      
+
+						        else                                                        
+
+								                printf("%i is zero\n", n);                          
+
+							        return (0)
