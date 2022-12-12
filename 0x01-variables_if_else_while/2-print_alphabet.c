c918c0a24286e70d969f73b56bcef08209e35640
Liuelg <Liueltekagirma@gmail.com>
2022-12-12 12:57:23 -0800

    question nember 0

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,15 @@
 /*
  * main - main block
  * Description: Get a random it with 5
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
-		putchar(ch);
+		putchar(c);
 		c++;
 	}
 
